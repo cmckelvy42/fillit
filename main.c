d0cd33aa261770903d6314ed09b071665e3e1f8e
Christopher Mckelvy <cmckelvy@e1z2r8p17.42.us.org>
2019-03-10 22:54:05 -0700

    proper verification

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cmckelvy <cmckelvy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 13:59:29 by cmckelvy          #+#    #+#             */
-/*   Updated: 2019/03/10 22:50:40 by cmckelvy         ###   ########.fr       */
+/*   Updated: 2019/03/10 22:53:56 by cmckelvy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int		main(int argc, char *argv[])
 {
-	int i = 0;
-
 	if (argc != 2)
 	{
 		ft_putstr("usage: fillit <file>\n");
